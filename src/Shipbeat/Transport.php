debc6694985c787ff0ee9b5cacaea91c8daf2bd4
Plamen Kojuharov <plamen.kojuharov@paymill.de>
2015-01-09 18:49:15 +0200

    Transport class php doc added. Implementation updated.

--- src/Shipbeat/Transport.php
+++ src/Shipbeat/Transport.php
@@ -1,11 +1,28 @@
 <?php
 
+/**
+ * Class Shipbeat_Transport
+ */
 class Shipbeat_Transport
 {
+    /**
+     * @var string
+     */
     private $token;
+    /**
+     * @var string
+     */
     private $domain;
+    /**
+     * @var string
+     */
     private $mode;
 
+    /**
+     * @param $token
+     * @param $mode
+     * @param $domain
+     */
     function __construct($token, $mode, $domain)
     {
         $this->token = $token;
@@ -13,20 +30,37 @@ class Shipbeat_Transport
         $this->domain = $domain;
     }
 
+    /**
+     * @param $endpoint
+     * @param null $parameters
+     * @return array
+     */
     public function get($endpoint, $parameters = null)
     {
         return $this->baseRequestMethod($endpoint, 'GET', $parameters);
     }
 
+    /**
+     * @param $endpoint
+     * @param null $postFields
+     * @return array
+     */
     public function post($endpoint, $postFields = null)
     {
         return $this->baseRequestMethod($endpoint, 'POST', $postFields);
     }
 
+    /**
+     * @param $endpoint
+     * @param $method
+     * @param null $parameters
+     * @return array
+     */
     private function baseRequestMethod($endpoint, $method, $parameters = null)
     {
         $ch = curl_init();
 
+        // prepare parameters
         if (!is_null($parameters)) {
             $queryParameters = http_build_query($parameters);
             if ($method == 'POST')
@@ -35,6 +69,7 @@ class Shipbeat_Transport
                 $endpoint = $endpoint . '?' . $queryParameters;
         }
 
+        // set verification only in live mode
         if ($this->mode == 'test')
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         else
@@ -48,21 +83,26 @@ class Shipbeat_Transport
         curl_setopt($ch, CURLOPT_HEADER, 1);
         curl_setopt($ch, CURLINFO_HEADER_OUT, true);
 
-        $result = curl_exec($ch);
+        $result = $this->curl_exec($ch);
 
-        $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $code = $this->curl_getinfo($ch, CURLINFO_HTTP_CODE);
         $headers = $this->getHeaders($result);
-        $body = json_decode(substr($result, curl_getinfo($ch, CURLINFO_HEADER_SIZE)));
+        $body = json_decode(substr($result, $this->curl_getinfo($ch, CURLINFO_HEADER_SIZE)));
 
         curl_close($ch);
 
-        $response = array('code' => $code, 'response' => $body, true);
+        // create response with total_count if exists
+        $response = array('code' => $code, 'response' => $body);
         if (array_key_exists('X-Total-Count', $headers))
             $response['total_count'] = $headers['X-Total-Count'];
 
         return $response;
     }
 
+    /**
+     * @param $response
+     * @return array
+     */
     private function getHeaders($response)
     {
         $headers = array();
@@ -79,5 +119,24 @@ class Shipbeat_Transport
             }
         }
         return $headers;
+    }
+
+    /**
+     * @param $ch
+     * @return mixed
+     */
+    protected function curl_exec($ch)
+    {
+        return curl_exec($ch);
+    }
+
+    /**
+     * @param $ch
+     * @param $option
+     * @return mixed
+     */
+    protected function curl_getinfo($ch, $option)
+    {
+        return curl_getinfo($ch, $option);
     }
 }
